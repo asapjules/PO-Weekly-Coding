176e6527051bd7385023c3a7d63dda4ca51bdcb5
asapjules <asapjules1@gmail.com>
2021-09-21 19:40:39 -0400

    added smoothie

---
+++ Smoothie/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace SmoothieProgram
+{
+    class Program
+    {   
+        static void Main(string[] args)
+        {
+            Smoothie s1 = new Smoothie(new string[] { "Banana" });
+            Console.WriteLine(s1.GetCost());
+            Console.WriteLine(s1.GetPrice());
+            Console.WriteLine(s1.GetName());
+            Console.WriteLine(s1.Ingredients);
+
+            Smoothie s2 = new Smoothie(new string[] { "Raspberries", "Strawberries", "Blueberries" });
+            Console.WriteLine(s2.Ingredients);
+            Console.WriteLine(s2.GetCost());
+            Console.WriteLine(s2.GetPrice());
+            Console.WriteLine(s2.GetName());
+        }
+    }
+}
+
---
+++ Smoothie/Smoothie.cs
@@ -0,0 +1,84 @@
+﻿using System;
+
+namespace SmoothieProgram
+{
+    public class Smoothie
+    {
+        public string Ingredients;
+        private string[] IngredientsList;
+        private double cost;
+        public Smoothie(string[] ingredients){
+        IngredientsList = ingredients;
+        Array.Sort(IngredientsList);
+            foreach(var ingredient in IngredientsList){
+                if (ingredient == "Banana"){
+                    cost += 0.50;
+                }
+                else if(ingredient == "Strawberries"){
+                    cost+=1.50;
+                }
+                else if(ingredient == "Mango"){
+                    cost+= 2.50;
+                }
+                else if(ingredient == "Blueberries"){
+                    cost+= 1.00;
+                }
+                else if(ingredient == "Raspberries"){
+                    cost+= 1.00;
+                }
+                else if (ingredient == "Apple"){
+                    cost += 1.75;
+                }
+                else if( ingredient == "Pineapple"){
+                    cost += 3.5;
+                }
+                else{
+                    cost +=0;
+                }
+            }
+        Ingredients = string.Join(" ", ingredients);
+        Ingredients = "{ "+ Ingredients + " }";
+        }
+        public double GetCost(){
+            return cost;
+        }
+        public double GetPrice(){
+            double initial = this.GetCost();
+            return Math.Round(initial + initial*1.5, 2);
+        }
+        public string GetName(){
+            string name = "";
+            foreach(var ingredient in IngredientsList){
+                if (ingredient == "Banana"){
+                    name += "Banana ";
+                }
+                else if(ingredient == "Strawberries"){
+                    name += "Strawberry ";
+                }
+                else if(ingredient == "Mango"){
+                    name += "Mango ";
+                }
+                else if(ingredient == "Blueberries"){
+                    name += "Blueberry ";
+                }
+                else if(ingredient == "Raspberries"){
+                    name += "Raspberry ";
+                }
+                else if (ingredient == "Apple"){
+                    name += "Apple ";
+                }
+                else if( ingredient == "Pineapple"){
+                    name += "Pineapple ";
+                }
+            }
+            if(Ingredients.Length > 1){
+                name += "Fusion";
+            }
+            else{
+                name += "Smoothie";
+            }
+            return name;
+        }
+    }
+
+}
